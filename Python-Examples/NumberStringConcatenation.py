55db192ab10d2916bd3ccc7b05995f4b5a7b11ce
Rodeorat316 <alex@hannahtechnologies.com>
2020-06-09 00:46:48 -0400

    Second Commit 6-9-20

---
+++ Python-Examples/NumberStringConcatenation.py
@@ -0,0 +1,13 @@
+print("This is an example of a print statement")
+myName = "Alex"
+myAge = 39
+print(myName)
+print(myAge)
+print("Another way to do this is with String Concatentation")
+print("Example here is, the persons name is: "+ myName +" cool eh?")
+print("And this persons age is: " + str(myAge) + "years old!")
+print("End of example")
+
+
+#Note that in order to cast or convert a Integer to a String you use the function "str(Variable Name)"
+#We had to do this when we concatenated the strings and integers together as this was inside a print statement that needed a string
---
+++ Python-Examples/StringConcatenation.py
@@ -0,0 +1,9 @@
+print("This is an example of a print statement")
+myName = "Alex"
+myAge = "39"
+print(myName)
+print(myAge)
+print("Another way to do this is with String Concatentation")
+print("Example here is, the persons name is: "+ myName +" cool eh?")
+print("And this persons age is: " + myAge + "years old!")
+print("End of example")
